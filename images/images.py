76be6939fc0665051b88620ad0370bb9a27a655d
Shelinnaxie <shelinnahshelinna1208@gmail.com>
2024-12-10 15:53:06 +0700

    select difficulty

--- images/images.py
+++ images/images.py
@@ -7,7 +7,7 @@ def load_image():
     # Image Path
     script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 
-    # Title Screen Image
+    # difficulty Screen Image
     image_path = "hangman.jpg"
     abs_image_path = os.path.join(script_dir, image_path)
     titleImage = Image.open(abs_image_path)
@@ -30,5 +30,34 @@ def load_image():
     exitImage = exitImage.resize((60,60))
     exitPhoto = ImageTk.PhotoImage(exitImage)
     images['exitPhoto'] = exitPhoto
+
+    image_path = "select difficulty.jpg"
+    abs_image_path = os.path.join(script_dir, image_path)
+    difficultyImage = Image.open(abs_image_path)
+    difficultyImage = difficultyImage.resize((200,100))
+    difficultyPhoto = ImageTk.PhotoImage(difficultyImage)
+    images['difficultyPhoto'] = difficultyPhoto
+
+    image_path = "easy button.jpg"
+    abs_image_path = os.path.join(script_dir, image_path)
+    easyImage = Image.open(abs_image_path)
+    easyImage = easyImage.resize((60,60))
+    easyPhoto = ImageTk.PhotoImage(easyImage)
+    images['easyPhoto'] = easyPhoto
+
+    image_path = "medium button.jpg"
+    abs_image_path = os.path.join(script_dir, image_path)
+    mediumImage = Image.open(abs_image_path)
+    mediumImage = mediumImage.resize((60,60))
+    mediumPhoto = ImageTk.PhotoImage(mediumImage)
+    images['mediumPhoto'] = mediumPhoto
+
+    image_path = "hard button.jpg"
+    abs_image_path = os.path.join(script_dir, image_path)
+    hardImage = Image.open(abs_image_path)
+    hardImage = hardImage.resize((60,60))
+    hardPhoto = ImageTk.PhotoImage(hardImage)
+    images['hardPhoto'] = hardPhoto
+
 
     return images
